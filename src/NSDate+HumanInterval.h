326d927e1d38e2a726667faeed3eb32a40ce8e3a
David E. Wheeler <david@justatheory.com>
2011-06-06 17:03:58 -0700

    Update copyright notices.

--- src/NSDate+HumanInterval.h
+++ src/NSDate+HumanInterval.h
@@ -3,7 +3,7 @@
 //  Buzzalot
 //
 //  Created by David E. Wheeler on 2/18/10.
-//  Copyright 2010 Kineticode, Inc.. All rights reserved.
+//  Copyright 2010-2011 Lunar/Theory. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
